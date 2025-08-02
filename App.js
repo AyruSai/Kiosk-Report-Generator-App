9a6352beb068b72e33c658e22f44b705a476df3f
gsaisuryasasidhar <gsaisuryasasidhar@gmail.com>
2025-08-02 12:24:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './app/pages/LoginPage';
+
+
+
+export default function App() {
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app! sdfghjkhgfdsadfghjkjhg</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <>
+  <LoginPage/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/pages/LoginPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginPage = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Anudeep</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',  
+        alignItems: 'center',
+    },
+    text: {
+        color: 'red',
+        fontSize: 24,
+    },
+});
+
+export default LoginPage;
